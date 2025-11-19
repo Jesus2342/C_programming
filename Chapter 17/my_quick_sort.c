b2df8e556d96b7a81d64bd26ea3c6a9641ea82e2
Jesus Pena <penajesus011@gmail.com>
2025-11-19 15:33:46 -0700

    uploaded ex19

--- Chapter 17/my_quick_sort.c
+++ Chapter 17/my_quick_sort.c
@@ -1,53 +1,55 @@
 #include <stdio.h>
 
+void my_qs(int arr[], int low, int high);
+int partition(int arr[], int low, int high);
+void print_array(int n, int arr[n]);
 
-void my_qs(int arr[], int n);
-
-
-
-int main(){
-
+int main() {
     int arr[] = {8, 5, 23, 1, 12};
     int n = sizeof(arr) / sizeof(arr[0]);
-    n -= 1;
 
-    //printf("%d",n);
-    my_qs(arr, n);
+    my_qs(arr, 0, n - 1);
 
-
+    print_array(n, arr);
     return 0;
 }
 
-void my_qs(int arr[], int n){
+// Recursive quicksort
+void my_qs(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot_index = partition(arr, low, high);
 
-    int i = -1;
-    int j = 0;
-    int *p = arr;
-    int pivot = arr[n];
-    int temp;
-
-
-
-    while(j < n){
-        if(*p < pivot){
-            i++;
-            temp = arr[j];
-            arr[j] = arr[i];
-            arr[i] = temp;
-            
-        }
-        p++;
-        j++;
-        
+        // Recursively sort left and right parts
+        my_qs(arr, low, pivot_index - 1);
+        my_qs(arr, pivot_index + 1, high);
     }
-
-    temp = arr[n]; //pivot
-    arr[n] = arr[i + 1];
-    arr[i + 1] = temp;  
-
-    for(int k = 0; k <= n; k++){
-        printf("%d ",arr[k]);
-    }
-
 }
 
+// Partition function (Lomuto scheme)
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // Place pivot in correct position
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+void print_array(int n, int arr[n]) {
+    for (int k = 0; k < n; k++) {
+        printf("%d ", arr[k]);
+    }
+    printf("\n");
+}
